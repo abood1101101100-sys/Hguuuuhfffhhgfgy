4ced43769cb9fb2c6dbdc40482edcd4593540bca
abood1101101100-sys <abood1101101100@gmail.com>
2026-06-22 09:15:08 +0300

    Update bot.py

--- SORSUE Alqaid/bot.py
+++ SORSUE Alqaid/bot.py
@@ -1,9 +1,14 @@
 import os
+import sys
 from pyrogram import Client, idle
 from pyromod import listen
 from casery import caes, casery, group, source, photosource, caserid, ch, bot_token, bot_token2
 
-bot = Client("CAR", api_id=15646135, api_hash="b38142278f5181e0815bf7122478dc31", bot_token=bot_token, plugins=dict(root=os.path.join(os.path.dirname(__file__), "CASER")))
+# تغيير مجلد العمل لمجلد البوت
+sys.path.insert(0, os.path.dirname(__file__))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+bot = Client("CAR", api_id=15646135, api_hash="b38142278f5181e0815bf7122478dc31", bot_token=bot_token, plugins=dict(root="CASER"))
 lolo = Client("hossam", api_id=15646135, api_hash="b38142278f5181e0815bf7122478dc31", session_string=bot_token2)    
 
 DEVS = caes
