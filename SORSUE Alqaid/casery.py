3d30b5e0536af72dac5696bf7070951d617f9978
abood1101101100-sys <abood1101101100@gmail.com>
2026-06-22 09:09:53 +0300

    Update casery.py

--- SORSUE Alqaid/casery.py
+++ SORSUE Alqaid/casery.py
@@ -11,6 +11,6 @@ group = "https://t.me/ueheurhd"
 ##لوجو السورس
 photosource = "https://t.me/mkptkm/14"
 ##توكن الصانع
-bot_token="8868801260:AAGQ4poVHo-xUkYM5mBgZqmOC1O6DApDrHw"
+bot_token="8868801260:AAHgB4hiwqGeMqgcxnCGLZy4M-AbXsK2_r8"
 ##جلسه الاشتراك
 bot_token2="AQDuvbcAhqgwbFA-G3Gic4coETKZk8FD9AxuDqEs3CsamWHhubZmyos7xd31slWAGIEcu4q8iTLtSuQFfF1y75o9H7I6kGIN57S-4QEW2t_7ZQskVTf094EM6D6lXPcKERuAZLscQIwXvcBdCH7fN5_X3cMr1wf4vmeOEVs6sBbb5Y9gG9zwRI7UmxNRcKhSOGtbdMqkE-GUpBVXDWo8DOW1TI-lwljSQAcM4lFDMsBk_zRy9YMgP-QZ8BKc2eXH3m3Ps_wWmD1jA7vAdzdYdK-XGdUYAi7eHS6KsEpGSlX8V6CSRW4A5KdmqZvOu2WYeH8ZL3pB0-ezkxW2Fb0HJziBvYse9wAAAAIIAr4sAA"
