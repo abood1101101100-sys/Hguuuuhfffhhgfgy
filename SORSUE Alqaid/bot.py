5085a119b3f8cfa21b58e1f143b0bc706e2f2e00
abood1101101100-sys <abood1101101100@gmail.com>
2026-06-22 09:08:17 +0300

    Update bot.py

--- SORSUE Alqaid/bot.py
+++ SORSUE Alqaid/bot.py
@@ -1,9 +1,10 @@
+import os
 from pyrogram import Client, idle
 from pyromod import listen
 from casery import caes, casery, group, source, photosource, caserid, ch, bot_token, bot_token2
 
-bot = Client("CAR", api_id=8186557, api_hash="efd77b34c69c164ce158037ff5a0d117", bot_token=bot_token, plugins=dict(root="CASER"))
-lolo = Client("hossam", api_id=8186557, api_hash="efd77b34c69c164ce158037ff5a0d117", session_string=bot_token2)    
+bot = Client("CAR", api_id=15646135, api_hash="b38142278f5181e0815bf7122478dc31", bot_token=bot_token, plugins=dict(root=os.path.join(os.path.dirname(__file__), "CASER")))
+lolo = Client("hossam", api_id=15646135, api_hash="b38142278f5181e0815bf7122478dc31", session_string=bot_token2)    
 
 DEVS = caes
 DEVSs = []
